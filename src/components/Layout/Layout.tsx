7a7b983bd2695b177bfe85b2ad00f733a37aa7f2
Jose Rogerio de Almeida Junior <almeidajr.vr@gmail.com>
2021-12-03 17:36:45 -0300

    Display current version

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,11 +2,12 @@ import ProLayout from '@ant-design/pro-layout'
 import { BackTop } from 'antd'
 import { Link, Outlet, useLocation } from 'react-router-dom'
 
+import packageJson from '../../../package.json'
 import { useAuth } from '../../hooks/useAuth'
 import { paths } from '../../routes/paths'
 import { UserAvatar } from '../UserAvatar'
 import { route } from './route'
-import { Container } from './styles'
+import { Container, Version } from './styles'
 
 export const Layout = () => {
   const { isAuthenticated } = useAuth()
@@ -19,6 +20,7 @@ export const Layout = () => {
         headerRender={isAuthenticated ? undefined : false}
         hide={!isAuthenticated}
         location={location}
+        menuFooterRender={() => <Version code>v{packageJson.version}</Version>}
         menuItemRender={(item, dom) => (
           <Link to={item.path ?? paths.purchasesHistoric}>{dom}</Link>
         )}
--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,5 +1,23 @@
 import styled from '@emotion/styled'
+import { Typography } from 'antd'
+
+const { Text } = Typography
 
 export const Container = styled.div`
   height: 100vh;
+`
+export const Version = styled(Text)`
+  display: block;
+  width: 100%;
+  color: #fff;
+
+  code {
+    display: block;
+    width: 100%;
+    border-radius: 0;
+    margin: 0;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    text-align: center;
+  }
 `
